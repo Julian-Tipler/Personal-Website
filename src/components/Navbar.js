0a8a71fed1ced7b139c71209905c84c7788025ed
Julian <tipler.julian@gmail.com>
2021-04-23 18:02:37 -0700

    simplify navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,36 +1,37 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 function Navbar() {
-    const [click, setClick] = useState(false);
+  const [click, setClick] = useState(false);
 
-    const handleClick = () => {
-        console.log('hello')
-        setClick(!click);
-    }
-    const closeMobileMenu = () => setClick(false);
-    
-    return (
-        <>
-            <nav className='navbar'>
-                <div className='navbar-container'>  
-                <Link to="/" className="navbar-logo">
-                    <img/>
-                </Link>
-                <div className='menu-icon' onClick={handleClick}>
-                    <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
-                </div>
-                <ul className={click ? 'nav-menu active' : 'nav-menu'}>
-                    <li className='nav-item'>
-                        <Link to='/' className='nav-links' onClick={closeMobileMenu}>
-                            Home
-                        </Link>
-                    </li>
-                </ul>
-                </div>
-            </nav>
-        </>  
-    )
+  const handleClick = () => {
+    setClick(!click);
+  };
+  const closeMobileMenu = () => setClick(false);
+
+  return (
+    <div>
+      <nav className="navbar">
+        <div className="navbar-container">
+          <Link to="/" className="navbar-logo">
+            <img />
+          </Link>
+          <ul className={click ? "nav-menu active" : "nav-menu"}>
+            <li className="nav-item">
+              <Link to="/" className="nav-links">
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/" className="nav-links">
+                Portfolio
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  );
 }
 
-export default Navbar
+export default Navbar;
